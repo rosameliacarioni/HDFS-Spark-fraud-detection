574b745513bb1cc84f7a07585c3a32262f13ca4c
Jonas <jonas.mueller.contact@gmail.com>
2024-10-19 14:56:38 +0200

    Add more info logs to start-hadoop

--- infra/hdfs/start-hadoop.sh
+++ infra/hdfs/start-hadoop.sh
@@ -14,6 +14,7 @@ echo "export  HADOOP_USER_NAME=\"${HDFS_USER}\"" >> /etc/bash.bashrc
 if ! id -u $HDFS_USER > /dev/null 2>&1; then
   adduser --no-create-home --disabled-password --gecos "" $HDFS_USER
 fi
+echo "User $HDFS_USER created/exists"
 # Fix directory permissions
 if ! [[ $(whoami) = $HDFS_USER ]]; then
   find /opt/hdfs ! -user $HDFS_USER -print0 | xargs -0 chown $HDFS_USER:$HDFS_USER \
@@ -24,12 +25,14 @@ else
   echo "\$HDFS_USER is root"
 fi
 
-echo "User $HDFS_USER created"
+echo "HDFS_USER permissions fixed"
 
 # Select node type
 if [[ "${1}" = 'namenode' ]]; then
+  echo "Starting Hadoop NameNode"
   su $HDFS_USER -c "/opt/util/bin/start-hadoop-namenode $2"
 elif [[ "${1}" =  'datanode' ]]; then
+  echo "Starting Hadoop DataNode"
   su $HDFS_USER -c "/opt/util/bin/start-hadoop-datanode $2"
 else
   echo "Invalid command '${1}'" >&2
